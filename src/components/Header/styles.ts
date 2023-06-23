c1bd61814237713a69bcdba5f15991b3883ed1ce
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-06-24 00:00:00 +0100

    Create basic header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  align-items: center;
+  width: 100%;
+  flex-direction: row;
+`;
+
+export const LogoContainer = styled.View`
+  flex-direction: row;
+  gap: 5px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LogoIcon = styled.Image`
+  width: 37px;
+  height: 37px;
+`;
+
+export const LogoName = styled.Image`
+  width: 36px;
+  height: 31px;
+`;
