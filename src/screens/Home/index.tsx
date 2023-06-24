a02829232a51639aa7a80acf7f29a49f4c6db4e6
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-06-25 00:00:00 +0100

    Add Header component to Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,10 @@
+import { Header } from '@components/Header';
 import { Container, Title } from './styles';
 
 export function Home() {
   return (
     <Container>
+      <Header />
       <Title>Home</Title>
     </Container>
   )
