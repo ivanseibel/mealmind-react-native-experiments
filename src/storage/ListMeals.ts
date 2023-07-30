e7a160096bf56175d1e587a5df9e21d0ba9dcb46
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-07-31 00:00:00 +0100

    Remove unused import in ListMeals.ts

--- src/storage/ListMeals.ts
+++ src/storage/ListMeals.ts
@@ -1,7 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Meal } from "./MealStorageDTO";
 import { MEALS_COLLECTION } from "./config";
-import { AppError } from "@utils/AppError";
 
 
 export async function listMeals() {
