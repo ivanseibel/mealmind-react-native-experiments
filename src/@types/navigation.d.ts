95077ea6c70aaeb71d2ca356d7d84364b8e5ecc1
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-01-03 22:23:09 +0000

    Add types for routes

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined;
+      Statistics: undefined;
+      MealDetails: undefined;
+      MealForm: undefined;
+      Feedback: undefined;
+    }
+  }
+}
