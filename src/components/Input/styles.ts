429b77c1a6a15106767901ca25dcb2d21a03d707
Ivan Seibel <me@ivanseibel.dev>
2024-02-03 18:06:52 +0000

    Create Input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,21 @@
+import { TextInput } from "react-native";
+import styled, { css } from "styled-components/native";
+
+export const Container = styled(TextInput).attrs(({ theme }) => ({
+  placeholderTextColor: theme.COLORS.GRAY_3
+}))`
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    border-width: 1px;
+    border-color: ${theme.COLORS.GRAY_5};
+    color: ${theme.COLORS.GRAY_1};
+  `}
+
+  flex: 1;
+  width: 100%;
+  min-height: 48px;
+  max-height: 48px;
+  padding: 14px;
+  border-radius: 8px;
+`;
