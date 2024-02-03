2dc0a8bfeeab6e24417818c73a38ab1b929ef779
Ivan Seibel <me@ivanseibel.dev>
2024-02-03 18:07:52 +0000

    Create MealForm screen

--- src/screens/MealForm/index.tsx
+++ src/screens/MealForm/index.tsx
@@ -1,9 +1,44 @@
-import { Container, Title } from './styles';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { BackContainer, BackIcon, Container, HeaderContainer, Title } from './styles';
+import { Input } from '@components/Input';
+import { InputLabel } from '@components/InputLabel';
+
+type RouteParams = {
+  operation: 'create' | 'edit';
+}
 
 export function MealForm() {
+  const navigation = useNavigation();
+
+  const route = useRoute();
+  const { operation } = route.params as RouteParams;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Container>
-      <Title>MealForm</Title>
-    </Container>
+    <>
+      <Container
+        variant='header'
+      >
+        <HeaderContainer>
+          <BackContainer
+            onPress={handleGoBack}
+          >
+            <BackIcon />
+          </BackContainer>
+          <Title>{operation === 'create' ? 'New meal' : 'Edit meal'}</Title>
+        </HeaderContainer>
+      </Container>
+      <Container
+        variant='body'
+      >
+        <InputLabel 
+          title='Name'
+        />
+        <Input />
+      </Container>
+    </>
   )
 }
--- src/screens/MealForm/styles.ts
+++ src/screens/MealForm/styles.ts
@@ -1,13 +1,55 @@
-import stiled from 'styled-components/native';
+import { ArrowLeft } from 'phosphor-react-native';
+import styled, { css } from 'styled-components/native';
 
-export const Container = stiled.View`
-  flex: 1;
-  justify-content: center;
+type ContainerProps = {
+  variant: 'header' | 'body';
+}
+
+export const Container = styled.View<ContainerProps>`
+  ${({ variant }) => variant === 'header' && css`
+    background-color: ${({ theme }) => theme.COLORS.GRAY_5};
+    height: 156px;
+    justify-content: center;
+    padding: 0 24px;
+  `}
+  ${({ variant }) => variant === 'body' && css`
+    background-color: ${({ theme }) => theme.COLORS.WHITE};
+    margin-top: -32px;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+    flex: 1;
+    padding: 24px;
+  `}
   align-items: center;
 `;
 
-export const Title = stiled.Text`
-  font-size: 24px;
-  color: #000;
-  font-weight: bold;
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  font-size: ${({ theme }) => theme.FONT_SIZE.LG}px;
+  color: ${({ theme }) => theme.COLORS.GRAY_1};
+  line-height: ${({ theme }) => theme.FONT_SIZE.LG * theme.LINE_HEIGHT.MULTIPLIER}px;
+  text-align: center;
+  width: 100%;
+`;
+
+export const HeaderContainer = styled.View`
+  position: relative;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const BackContainer = styled.TouchableOpacity`
+  /* width: 100%; */
+  /* align-items: flex-start; */
+  /* border-radius: 8px; */
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+`;
+
+export const BackIcon = styled(ArrowLeft).attrs(({ theme }) => ({
+  color: theme.COLORS.GRAY_1,
+  size: 24,
+}))`
 `;
