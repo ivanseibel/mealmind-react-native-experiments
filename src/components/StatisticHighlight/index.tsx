833bdb39ee34703b2773b31a44d3c1844bbef557
Ivan Seibel <me@ivanseibel.dev>
2024-02-03 16:39:19 +0000

    Add back button to HighLight component

--- src/components/StatisticHighlight/index.tsx
+++ src/components/StatisticHighlight/index.tsx
@@ -1,26 +1,43 @@
-import { TouchableOpacityProps } from "react-native";
-import { Container, Icon, Percentage, Subtitle } from "./styles";
+import { TouchableOpacityProps, Text, TouchableOpacity } from "react-native";
+import { BackContainer, BackIcon, Container, HighLightIcon, Percentage, Subtitle } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 type StatisticHighlightProps = TouchableOpacityProps & {
   percentage: number;
-  variant: 'positive' | 'negative';
+  variant: 'positive' | 'negative' | 'positiveBack' | 'negativeBack';
 }
 
 export function StatisticHighlight({ percentage, variant, ...rest }: StatisticHighlightProps) {
+  const navigation = useNavigation();
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
   return (
     <Container
       variant={variant}
       activeOpacity={0.8}
       {...rest}
     >
-      <Icon
-        variant={variant}
-      />
+      {!variant.includes('Back') 
+        ? (<HighLightIcon variant={variant} />)
+        : (
+          <BackContainer
+            onPress={handleBack}
+          >
+            <BackIcon 
+              variant={variant}
+            />
+          </BackContainer>
+        )
+      }
+      
       <Percentage>
         {percentage}%
       </Percentage>
       <Subtitle>
-        of meals aligned with the diet
+        of meals within the diet
       </Subtitle>
     </Container>
   )
