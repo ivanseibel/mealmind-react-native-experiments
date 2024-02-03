d526b6f0dba10e4db2319fc05a54ff83ed4d87bb
Ivan Seibel <me@ivanseibel.dev>
2024-02-03 14:05:43 +0000

    Finish Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,8 @@
 import { Header } from '@components/Header';
-import { Container, DaysSectionList, MealsContainer, NewContainer, Subtitle } from './styles';
+import { Container, MealsContainer, NewContainer, SectionHeaderTitle, Subtitle } from './styles';
 import { StatisticHighlight } from '@components/StatisticHighlight';
 import { Button } from '@components/Button';
-import { View, StyleSheet, Text, SectionListRenderItemInfo, SectionListRenderItem, SectionList, DefaultSectionT, SectionListData } from 'react-native';
+import { SectionListRenderItemInfo, SectionList, DefaultSectionT, SectionListData } from 'react-native';
 import { MealSectionListItem } from '@components/MealSectionListItem';
 
 interface MealItem {
@@ -46,7 +46,36 @@ const MEALS: MealSection[] = [
         time: '20:00',
       },
     ]
-  }
+  },
+  {
+    title: '20.09.2021',
+    data: [
+      {
+        id: '1',
+        title: 'Breakfast',
+        time: '08:00',
+        status: 'red',
+      },
+      {
+        id: '2',
+        title: 'Lunch',
+        status: 'green',
+        time: '12:00',
+      },
+      {
+        id: '3',
+        title: 'Dinner',
+        status: 'red',
+        time: '18:00',
+      },
+      {
+        id: '4',
+        title: 'Snack Supper Breakfast Lunch Dinner',
+        status: 'green',
+        time: '20:00',
+      },
+    ]
+  },
 ]
 
 
@@ -64,37 +93,39 @@ export function Home() {
 
   function renderMealSection(section: SectionListData<MealItem, DefaultSectionT>) {
     return (
-      <Text>{section.title}</Text>
+      <SectionHeaderTitle>{section.title}</SectionHeaderTitle>
     )
   }
 
   return (
-    <Container>
-      <Header />
-      <StatisticHighlight 
-        percentage={90.86}
-        variant="positive"
-        style={{ marginTop: 36 }}
-      />
-      <MealsContainer>
-        <NewContainer>
-          <Subtitle>Meals</Subtitle>
-          <Button
-            label='Add meal'
-            onClick={() => {}}
-            variant='primary'
-            icon='plus'
-          />
+      <Container>
+        <Header />
+        <StatisticHighlight 
+          percentage={90.86}
+          variant="positive"
+          style={{ marginTop: 36 }}
+        />
+        <MealsContainer>
+          <NewContainer>
+            <Subtitle>Meals</Subtitle>
+            <Button
+              label='Add meal'
+              onClick={() => {}}
+              variant='primary'
+              icon='plus'
+            />
 
-          <SectionList 
-            style={{ width: '100%' }}
-            sections={MEALS}
-            keyExtractor={(item) => item.id }
-            renderItem={renderMealItem}
-            renderSectionHeader={({ section }) => renderMealSection(section)}
-          />
-        </NewContainer>
-      </MealsContainer>
-    </Container>
+            <SectionList 
+              style={{ width: '100%' }}
+              sections={MEALS}
+              keyExtractor={(item) => item.id }
+              renderItem={renderMealItem}
+              renderSectionHeader={({ section }) => renderMealSection(section)}
+              stickySectionHeadersEnabled={false}
+              showsVerticalScrollIndicator={false}
+            />
+          </NewContainer>
+        </MealsContainer>
+      </Container>
   )
 }
