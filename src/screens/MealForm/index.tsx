c7bc24cb1335cc9c5a9b09498f2770f7a8945212
Ivan Seibel <me@ivanseibel.dev>
2024-02-04 16:36:41 +0000

    Finish adding the rest of the fields to the form

--- src/screens/MealForm/index.tsx
+++ src/screens/MealForm/index.tsx
@@ -1,13 +1,19 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { BackContainer, BackIcon, Container, HeaderContainer, Title } from './styles';
+import { BackContainer, BackIcon, Container, FullWidhtInputContainer, HalfWidhtInputContainer, HeaderContainer, Title, YesNoIndicator, YesNoSelectorContainer, YesNoText } from './styles';
 import { Input } from '@components/Input';
 import { InputLabel } from '@components/InputLabel';
+import { View } from 'react-native';
+import { Button } from '@components/Button';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
 
 type RouteParams = {
   operation: 'create' | 'edit';
 }
 
 export function MealForm() {
+  const [withinDiet, setWithinDiet] = useState<true | false | undefined>(undefined);
+
   const navigation = useNavigation();
 
   const route = useRoute();
@@ -34,10 +40,89 @@ export function MealForm() {
       <Container
         variant='body'
       >
-        <InputLabel 
-          title='Name'
-        />
-        <Input />
+        <FullWidhtInputContainer>
+          <InputLabel 
+            title='Name'
+          />
+          <Input />
+        </FullWidhtInputContainer>
+        
+        <FullWidhtInputContainer>
+          <InputLabel 
+            title='Description'
+          />
+          <Input 
+            multiline
+            numberOfLines={4}
+          />
+        </FullWidhtInputContainer>
+
+        <View style={{ 
+          flexDirection: 'row',
+          gap: 10,
+        }}>
+          <HalfWidhtInputContainer>
+            <InputLabel 
+              title='Date'
+            />
+            <Input 
+            />
+          </HalfWidhtInputContainer>
+          <HalfWidhtInputContainer>
+            <InputLabel 
+              title='Time'
+            />
+            <Input 
+            />
+          </HalfWidhtInputContainer>
+        </View>
+
+        <View style={{ 
+          flexDirection: 'row',
+          gap: 10,
+        }}>
+          <HalfWidhtInputContainer>
+            <InputLabel 
+              title='Is it within the diet?'
+            />
+            <YesNoSelectorContainer 
+              onPress={() => setWithinDiet(true)}
+              highlighted={withinDiet === true ? 'green' : 'none'}
+            >
+              <YesNoIndicator color='green'/>
+              <YesNoText>
+                Yes
+              </YesNoText>
+            </YesNoSelectorContainer> 
+          </HalfWidhtInputContainer>
+          <HalfWidhtInputContainer>
+          <InputLabel 
+              title=' '
+            />
+            <YesNoSelectorContainer
+              onPress={() => setWithinDiet(false)}
+              highlighted={withinDiet === false ? 'red' : 'none'}
+            >
+              <YesNoIndicator color='red'/>
+              <YesNoText>
+                No
+              </YesNoText>
+            </YesNoSelectorContainer> 
+          </HalfWidhtInputContainer>
+        </View>
+
+        <SafeAreaView style={{
+          flex: 1,
+          justifyContent: 'flex-end',
+          width: '100%',
+        }}>
+          <Button 
+            label='Record meal'
+            variant='primary'
+            onClick={() => {}}
+          />
+        </SafeAreaView>
+
       </Container>
     </>
   )
