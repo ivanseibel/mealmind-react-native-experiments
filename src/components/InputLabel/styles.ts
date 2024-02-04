52645860a124dfb66d171e29b30d73394796d4fe
Ivan Seibel <me@ivanseibel.dev>
2024-02-04 16:35:38 +0000

    Remove unnecessary margin

--- src/components/InputLabel/styles.ts
+++ src/components/InputLabel/styles.ts
@@ -3,7 +3,6 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   width: 100%;
   align-items: flex-start;
-  margin-bottom: 4px;
 `;
 
 export const Title = styled.Text`
