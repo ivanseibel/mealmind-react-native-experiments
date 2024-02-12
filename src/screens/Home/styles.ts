0acf606809c56e5b69c2f6499cd48dcab9ca42d3
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-12 22:25:54 +0000

    Imporove styling

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -38,7 +38,7 @@ export const SectionHeaderTitle = styled.Text`
   `}
   line-height: 23px;
   width: 100%;
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  text-shadow: 0px 3px 3px rgba(0, 0, 0, 0.25);
   margin-bottom: 8px;
   margin-top: 32px;
 `;
