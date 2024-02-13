b6e0ca1c93bffe7e465b85758dc1cd441dc46b6a
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-13 19:35:31 +0000

    Implement Settings page and threshold

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -14,6 +14,7 @@ export declare global {
       Feedback: {
         variant?: 'positive' | 'negative';
       };
+      Settings: undefined;
     }
   }
 }
---
+++ src/storage/GetSettings.ts
@@ -0,0 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { STORAGE_KEYS } from "./config";
+import { AppError } from "@utils/AppError";
+import { Settings } from "./SettingsStorageDTO";
+
+export async function getSettings() {
+  try {
+    const serialized = await AsyncStorage.getItem(STORAGE_KEYS);
+
+    if (!serialized) {
+      return null;
+    }
+
+    return JSON.parse(serialized) as Settings;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
