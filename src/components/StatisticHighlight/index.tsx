7eef53689d1c33ef6f236a950a58b0be492b5037
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-13 20:04:51 +0000

    Implement using percentage threshold from storage

--- src/components/StatisticHighlight/index.tsx
+++ src/components/StatisticHighlight/index.tsx
@@ -1,24 +1,24 @@
 import { TouchableOpacityProps } from "react-native";
 import { Container, HighLightIcon, Percentage, Subtitle } from "./styles";
+import { Loading } from "@components/Loading";
 
 type StatisticHighlightProps = TouchableOpacityProps & {
   percentage: number;
   variant: 'positive' | 'negative';
   showHighlightIcon?: boolean;
+  isLoading?: boolean;
 }
 
-export function StatisticHighlight({ percentage, variant, showHighlightIcon = true,...rest }: StatisticHighlightProps) {
+export function StatisticHighlight({ percentage, variant, showHighlightIcon = true, isLoading = false, ...rest }: StatisticHighlightProps) {
   return (
     <Container
       variant={variant}
       activeOpacity={0.8}
       {...rest}
     >
-      {showHighlightIcon && (
-        <HighLightIcon variant={variant} />
-      )}
+      {showHighlightIcon && (<HighLightIcon variant={variant} />)}
       <Percentage>
-        {percentage}%
+        {isLoading ? <Loading /> : `${percentage}%`}
       </Percentage>
       <Subtitle>
         of meals within the diet
