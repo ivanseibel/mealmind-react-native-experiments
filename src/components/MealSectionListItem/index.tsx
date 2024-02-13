f589ac0d760ed8f25a68fcb38ae142817a6acbe8
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-13 18:15:33 +0000

    Not necessary formatting time anymore

--- src/components/MealSectionListItem/index.tsx
+++ src/components/MealSectionListItem/index.tsx
@@ -1,12 +1,11 @@
 import { Meal } from "@storage/MealStorageDTO";
 import { Container, Divider, MealStatus, MealTime, MealName } from "./styles";
-import { format } from "date-fns";
 
 export function MealSectionListItem({ id, time, date, name, status }: Meal) {
   return (
     <Container>
       <MealTime>
-        {format(time, 'HH:mm')}
+        {time}
       </MealTime>
       <Divider />
       <MealName
