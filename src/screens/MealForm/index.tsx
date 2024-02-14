7a42540bb40e68c80f11b58dff3bc822eafb96ad
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-14 21:07:26 +0000

    Refactor MealFormScreen component

--- src/screens/MealForm/index.tsx
+++ src/screens/MealForm/index.tsx
@@ -4,12 +4,13 @@ import { Input } from '@components/Input';
 import { InputLabel } from '@components/InputLabel';
 import { Alert, View } from 'react-native';
 import { Button } from '@components/Button';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { DateTimeInput } from '@components/DateTimeInput';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { addMeal } from '@storage/AddMeal';
 import { AppError } from '@utils/AppError';
 import { format } from 'date-fns';
+import { DismissKeyboardView } from '@components/DismissKeyboardView';
 
 type RouteParams = {
   operation: 'create' | 'edit';
@@ -34,8 +35,8 @@ export function MealFormScreen() {
   async function handleRecordMeal() {
     try {
       await addMeal({
-        name: name,
-        description,
+        name: name.trim(),
+        description: description.trim(),
         date: format(date, 'yyyy-MM-dd'),
         time: format(time, 'HH:mm'),
         status: withinDiet === true ? 'green' : withinDiet === false ? 'red' : undefined,
@@ -56,113 +57,115 @@ export function MealFormScreen() {
   }
 
   return (
-    <Main edges={['top', 'left', 'right']}>
-      <Header>
-        <HeaderContent>
-          <HeaderBackContainer
-            onPress={handleGoBack}
-          >
-            <BackIcon />
-          </HeaderBackContainer>
-          <Title>{operation === 'create' ? 'New meal' : 'Edit meal'}</Title>
-        </HeaderContent>
-      </Header>
-      <Body edges={['bottom', 'left', 'right']}>
-        <FullWidhtInputContainer>
-          <InputLabel 
-            title='Name'
-          />
-          <Input 
-            value={name}
-            onChange={(e) => setName(e.nativeEvent.text)}
-          />
-        </FullWidhtInputContainer>
-        
-        <FullWidhtInputContainer>
-          <InputLabel 
-            title='Description'
-          />
-          <Input 
-            multiline
-            numberOfLines={4}
-            value={description}
-            onChange={(e) => setDescription(e.nativeEvent.text)}
-          />
-        </FullWidhtInputContainer>
-
-        <View style={{ 
-          flexDirection: 'row',
-          gap: 10,
-        }}>
-          <HalfWidhtInputContainer>
-            <InputLabel 
-              title='Date'
-            />
-            <DateTimeInput
-              value={date}
-              onChange={setDate}
-            />
-          </HalfWidhtInputContainer>
-          <HalfWidhtInputContainer>
-            <InputLabel 
-              title='Time'
-            />
-            <DateTimeInput 
-              onChange={setTime}
-              mode='time'
-              value={time}
-            />
-          </HalfWidhtInputContainer>
-        </View>
-
-        <View style={{ 
-          flexDirection: 'row',
-          gap: 10,
-        }}>
-          <HalfWidhtInputContainer>
-            <InputLabel 
-              title='Is it within the diet?'
-            />
-            <YesNoSelectorContainer 
-              onPress={() => setWithinDiet(true)}
-              highlighted={withinDiet === true ? 'green' : 'none'}
+    <DismissKeyboardView>
+      <Main edges={['top', 'left', 'right']}>
+        <Header>
+          <HeaderContent>
+            <HeaderBackContainer
+              onPress={handleGoBack}
             >
-              <YesNoIndicator color='green'/>
-              <YesNoText>
-                Yes
-              </YesNoText>
-            </YesNoSelectorContainer> 
-          </HalfWidhtInputContainer>
-          <HalfWidhtInputContainer>
-          <InputLabel 
-              title=' '
+              <BackIcon />
+            </HeaderBackContainer>
+            <Title>{operation === 'create' ? 'New meal' : 'Edit meal'}</Title>
+          </HeaderContent>
+        </Header>
+        <Body edges={['bottom', 'left', 'right']}>
+          <FullWidhtInputContainer>
+            <InputLabel 
+              title='Name'
             />
-            <YesNoSelectorContainer
-              onPress={() => setWithinDiet(false)}
-              highlighted={withinDiet === false ? 'red' : 'none'}
-            >
-              <YesNoIndicator color='red'/>
-              <YesNoText>
-                No
-              </YesNoText>
-            </YesNoSelectorContainer> 
-          </HalfWidhtInputContainer>
-        </View>
+            <Input 
+              value={name}
+              onChange={(e) => setName(e.nativeEvent.text)}
+            />
+          </FullWidhtInputContainer>
+          
+          <FullWidhtInputContainer>
+            <InputLabel 
+              title='Description'
+            />
+            <Input 
+              multiline
+              numberOfLines={4}
+              value={description}
+              onChange={(e) => setDescription(e.nativeEvent.text)}
+            />
+          </FullWidhtInputContainer>
 
-        <View style={{
-          flex: 1,
-          justifyContent: 'flex-end',
-          width: '100%',
-          paddingBottom: 20,
-        }}>
-          <Button 
-            label='Record meal'
-            variant='primary'
-            onClick={handleRecordMeal}
-          />
-        </View>
+          <View style={{ 
+            flexDirection: 'row',
+            gap: 10,
+          }}>
+            <HalfWidhtInputContainer>
+              <InputLabel 
+                title='Date'
+              />
+              <DateTimeInput
+                value={date}
+                onChange={setDate}
+              />
+            </HalfWidhtInputContainer>
+            <HalfWidhtInputContainer>
+              <InputLabel 
+                title='Time'
+              />
+              <DateTimeInput 
+                onChange={setTime}
+                mode='time'
+                value={time}
+              />
+            </HalfWidhtInputContainer>
+          </View>
 
-      </Body>
-    </Main>
+          <View style={{ 
+            flexDirection: 'row',
+            gap: 10,
+          }}>
+            <HalfWidhtInputContainer>
+              <InputLabel 
+                title='Is it within the diet?'
+              />
+              <YesNoSelectorContainer 
+                onPress={() => setWithinDiet(true)}
+                highlighted={withinDiet === true ? 'green' : 'none'}
+              >
+                <YesNoIndicator color='green'/>
+                <YesNoText>
+                  Yes
+                </YesNoText>
+              </YesNoSelectorContainer> 
+            </HalfWidhtInputContainer>
+            <HalfWidhtInputContainer>
+            <InputLabel 
+                title=' '
+              />
+              <YesNoSelectorContainer
+                onPress={() => setWithinDiet(false)}
+                highlighted={withinDiet === false ? 'red' : 'none'}
+              >
+                <YesNoIndicator color='red'/>
+                <YesNoText>
+                  No
+                </YesNoText>
+              </YesNoSelectorContainer> 
+            </HalfWidhtInputContainer>
+          </View>
+
+          <View style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+            width: '100%',
+            paddingBottom: 20,
+          }}>
+            <Button 
+              label='Record meal'
+              variant='primary'
+              onClick={handleRecordMeal}
+            />
+          </View>
+
+        </Body>
+      </Main>
+    </DismissKeyboardView>
   )
 }
